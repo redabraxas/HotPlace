d15a2485cc54228e5c6b93d61fd8683a47965796
redabraxas <chocoroll@ChocoRolls-MacBook-Air.local>
2015-11-20 17:42:16 +0900

    setting2

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ app = Flask(__name__)
 
 
 
+# 기본루트페이지
 @app.route('/')
+@app.route('/map/')
 def index():
-    return render_template('index.html')
+    return render_template('map.html')
 
 
-@app.route('/main/<name>')
+# 커뮤니티
+@app.route('/community/')
+@app.route('/community/<name>')
 def main(name=None):
-    return render_template('main.html', name=name)
+    return render_template('community.html', name=name)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+# 로그인
+@app.route('/login/')
+@app.route('/login/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('login.html', name=name)
 
+
+
+################### 예제 그냥 남겨놓은거 #################
 @app.route('/hello2/')
 @app.route('/hello2/<title>&<name>')
 def hello2(name=None, title=None):
@@ -31,7 +39,7 @@ def hello3():
     data=[dict(href="http://naver.com", caption="네이버"), 
           dict(href="http://www.google.com", caption="구글")]
     return render_template('hello3.html', items=data)
-
+######################################################
 
 
 if __name__ == '__main__':
